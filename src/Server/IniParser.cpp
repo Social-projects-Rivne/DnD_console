776866c8e4e28d2eefc8607863281d8004fe2aac
Stanislav Petruk <petrukstas@gmail.com>
2016-12-30 02:02:13 +0200

    Finished IniParser

--- src/Server/IniParser.cpp
+++ src/Server/IniParser.cpp
@@ -1,105 +1,116 @@
+#include "Includes/IniParser.hpp"
 
-
-#include "IniParser.hpp"
-
-IniParser::IniParser(std::string path) : _path(path)
+IniParser::IniParser(std::string path) : 
+	                            _path(path)
 {
 }
 
 std::map<std::string, std::string> IniParser::fGetParams()
 {
-	std::ifstream file(_path.c_str());
-	std::string buff;
-	std::string res = "";
-	std::string category_name;
-	std::string insert;
-	std::string key;
-	std::string value;
-	bool isKey = true;
+	std::ifstream file(_path.c_str());  // open file
+	std::string   buff;                 // buffer for line in file
+	std::string   category_name;        // category name in file [Example]
+	std::string   insert;               // insert variable with type Category.Key=Value
+	std::string   key;                  // map key
+	std::string   value;                // map value
+	bool          is_key = true;        // flag for checking
 
-	for (int i = 0; std::getline(file, buff); ++i)
+	if (file.is_open())
 	{
-		insert = "";
-		if (buff[0] == '[')
+		for (int i = 0; std::getline(file, buff); ++i)
 		{
-			category_name = "";
-			for (int ch = 1; ch < buff.length()-1; ch++)
-				category_name+=buff[ch];
+			insert = "";                      // reset insert value;
 
-			category_name += ".";
-			continue;
-		}
-
-		insert = category_name + buff;
-		key = "";
-		value = "";
-		for (int j = 0; j < insert.length(); j++)
-		{
-			if(insert[j] == ' ') continue;
-			if(insert[j] == '=')
+			if (buff.length() == 0) continue; // check if line is empty
+			if (buff[0] == '[')               // if new category detected update category name
 			{
-				isKey = false;
+				category_name = "";           // reset category_name if new finded
+				for (int ch = 1; ch < buff.length() - 1; ch++)
+					category_name += buff[ch];
+
+				category_name += ".";
 				continue;
 			}
-			if(isKey)
-				key+=insert[j];
-			else
-				value+=insert[j];
-		}
-		isKey = true;
-		_params.insert({key, value});
-	}
 
-	file.close();
+			insert = category_name + buff;
+			key = "";                         // reset key 
+			value = "";                       // reset value
+
+			for (int j = 0; j < insert.length(); j++)
+			{
+				if (insert[j] == ' ' || insert[j] == '"')
+					continue;                 // check for empty spaces and " signs
+				if (insert[j] == '=')         // if equal sign then trigger key flag to false
+				{
+					is_key = false;
+					continue;
+				}
+				if (is_key)                   // if key flag insert data in key
+					key += insert[j];
+				else
+					value += insert[j];       // if not key flag insert data in value
+			}
+			is_key = true;                    // reset key flag
+			_params.insert({ key, value });   // insert data into map
+		}
+		file.close();
+	}
+	else
+		throw "FileOpenException";
 	return _params;
 }
 
 
-void IniParser::fSetParams(std::map<std::string, std::string> params)
+void IniParser::fSetParams(std::map<std::string, std::string> new_params)
 {
-	std::fstream file(_path.c_str());
-	std::string map_category_name;
-	std::string tmp_key;
-	std::string key;
-	std::string value;
-	bool category_flag = true;
+	std::ofstream                      file(_path.c_str());  // open file
+	std::map<std::string, std::string> old_params;           // old file params
+	std::string                        map_category_name;    // category name
+	std::string                        tmp_category_name;    // tmp category name
+	std::string                        key;                  // map key without category
 
-
-	if (file.is_open())
+	if (file.is_open())                                      // check for file
 	{
-		for (auto &el : params)
+		old_params = fGetParams();                           // get old params
+		for (auto &el : new_params)                          // insert or update old params
 		{
-			tmp_key = el.first;
-			key = "";
-			map_category_name = "";
-			value = el.second;
+			auto param_value = old_params.find(el.first);
+			if (param_value == old_params.end())
+				old_params.insert(el);                       // insert new values
+			else
+				param_value->second = el.second;             // update old values
+		}
 
-			for (int i = 0; i < tmp_key.length(); i++)
+		tmp_category_name = "";
+		map_category_name = "";
+		for (auto &el : old_params)                          // write in file
+		{
+			tmp_category_name = "";
+			for (int i = 0; i < el.first.length(); i++)      // update category name
 			{
-				if (tmp_key[i] == '.')
-				{
-					category_flag = false;
-					continue;
-				}
-				if (category_flag)
-					map_category_name+=tmp_key[i];
-				else
-					key+=tmp_key[i];
+				if (el.first[i] == '.')
+					break;
+				tmp_category_name += el.first[i];
 			}
 
-			file << key << " = " << value << "\n";
+			if (map_category_name != tmp_category_name)      // check for new category
+			{
+				map_category_name = tmp_category_name;
+				file << "[" << map_category_name << "]\n";   // write new category
+			}
+
+			key = el.first;                                  // get key
+			key.erase(0, map_category_name.length() + 1);    // erase category from key
+			file << key << " = " << el.second << "\n";       // write in file 'key = value'
 		}
+		file.close();
 	}
 	else
-		std::cout << "An error occurred in file opening" << std ::endl;
-
-
-	file.close();
+		throw "FileOpenException";
 }
 
 
 IniParser::~IniParser()
 {
-	// TODO Auto-generated destructor stub
 }
 
