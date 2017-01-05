8772ede2d82f2c21b982cd99c6aa1b51674601ea
Olha <olhalesk@gmail.com>
2017-01-05 13:48:29 +0200

    Getting data from the Database

--- src/DataBase/GetData.cpp
+++ src/DataBase/GetData.cpp
@@ -1,55 +1,79 @@
-#include <iostream>
-#include <stdlib.h>
-#include <stdio.h>
+#include <my_global.h>
 #include <mysql.h>
-
-MYSQL *connection, mysql;
-MYSQL_RES *result;
-MYSQL_ROW row;
-int query_state;
+#include <iostream>
 
 using namespace std;
 
-void GetData();
+MYSQL* CONNECTION; // Database Connection Handle/Decryptor
+MYSQL_RES *RESULT; // Resulting Table Handle
+MYSQL_ROW ROW; // Rows Handle
 
-void PrintData();
+void fConnectionOpening();
+void fGetData(string user_name, string password);
 
 int main()
 {
-    GetData();
+    fConnectionOpening();
+    string user_name = "user1";
+    string password = "test";
+    fGetData(user_name, password);
     
-    return 0;
+    // Closing the connection
+    mysql_close(CONNECTION);
 }
 
-void GetData()
+void fConnectionOpening()
 {
-    bool received = false;
-    do
-    {
-        mysql_init(&mysql);
-        connection = mysql_real_connect(&mysql,"localhost", "root","qwer","cpp_data",3306,0,0);
-        if (connection == NULL)
-            cout << mysql_error(&amp;mysql) << endl;
-        
-        query_state = mysql_query(connection, "select user_count()");
-        if (query_state !=0)
-            cout << mysql_error(connection) << endl;
-        if (connection != NULL && ( row = mysql_fetch_row(result)) != NULL)
-            received = true;
-    } while(received != true);
+    // Get the Database Connection Handle
+    CONNECTION = mysql_init(NULL);
+    if (CONNECTION == NULL)
+        // If we can't create the Database Connection Handle
+        cout << "Error: can't create a MySQL Connection Handle.\n";
     
-    result = mysql_store_result(connection);
+    // Connecting to a Database Server
+    else if(!mysql_real_connect(CONNECTION, // address of an existing MYSQL structure.
+                                "localhost", // host name or an IP address
+                                "root", // user's MySQL login ID
+                                "root", // password for user
+                                "DnD", // database name
+                                NULL, // port number for the TCP/IP connection
+                                NULL, //  socket or named pipe to use
+                                0 // client flag
+                                ))
+        cout << "Error: can't connect to database.\n" << mysql_error(CONNECTION);
+    else
+        cout << "The connection is open." << endl;
     
-    PrintData();
-    
-    mysql_free_result(result);
-    mysql_close(connection);
 }
 
-void PrinData()
+void fGetData(string user_name, string password)
 {
-    while (( row = mysql_fetch_row(result)) != NULL)
+    string sql_statement = "SELECT username, password FROM Users WHERE username = '";
+    sql_statement += user_name;
+    sql_statement += "' AND password = '";
+    sql_statement += password;
+    sql_statement += "';";
+    
+    //Execute SQL-query
+    if (mysql_query(CONNECTION, sql_statement.c_str()) != 0)
+        cout << "Error: can't execute SQL-query\n";
+    
+    // Get a handle to the resulting table
+    RESULT = mysql_store_result(CONNECTION);
+    if (RESULT == NULL)
+        cout << "Error: can't get the result description\n";
+    else
     {
-        cout << row[0] << endl;
+        if (mysql_num_rows(RESULT) > 0)
+        {
+            // Go through all the records of the resulting table
+            while ((ROW = mysql_fetch_row(RESULT)) != NULL)
+                cout << ROW[0] << " " << ROW[1] << endl;
+        }
+        else if (mysql_num_rows(RESULT) == 0)
+            cout << "No user with these data.\n";
     }
+    
+    // Free up the memory used by the result table
+    mysql_free_result(RESULT);
 }
