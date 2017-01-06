ef2cb8ffab1c44380a11f4e3a1e354f91dee8308
OlhaLesk <olhalesk@gmail.com>
2017-01-06 10:33:20 +0200

    Update Database.cpp

--- src/DataBase/Includes/Database.cpp
+++ src/DataBase/Includes/Database.cpp
@@ -15,28 +15,28 @@ using namespace std;
 
 Database::Database()
 {
-    CONNECTION = NULL;
+    connection = NULL;
 }
 
 void Database::ConnectionOpening()
 {
     // Get the Database Connection Handle
-    CONNECTION = mysql_init(NULL);
-    if (CONNECTION == NULL)
+    connection = mysql_init(NULL);
+    if (connection == NULL)
         // If we can't create the Database Connection Handle
         cout << "Error: can't create a MySQL Connection Handle.\n";
     
     // Connecting to a Database Server
-    else if(!mysql_real_connect(CONNECTION, // address of an existing MYSQL structure.
+    else if(!mysql_real_connect(connection, // address of an existing MYSQL structure.
                                 "localhost", // host name or an IP address
-                                "olha", // user's MySQL login ID
-                                "success", // password for user
+                                "root", // user's MySQL login ID
+                                "root", // password for user
                                 "DnD", // database name
                                 NULL, // port number for the TCP/IP connection
                                 NULL, //  socket or named pipe to use
                                 0 // client flag
                                 ))
-        cout << "Error: can't connect to database.\n" << mysql_error(CONNECTION);
+        cout << "Error: can't connect to database.\n" << mysql_error(connection);
     else
         cout << "The connection is open." << endl;
 }
@@ -44,35 +44,35 @@ void Database::ConnectionOpening()
 void Database::GetUserData(string sql_statement)
 {
     //Execute SQL-query
-    if (mysql_query(CONNECTION, sql_statement.c_str()) != 0)
+    if (mysql_query(connection, sql_statement.c_str()) != 0)
         cout << "Error: can't execute SQL-query\n";
     
-    MYSQL_RES *RESULT; // Resulting Table Handle
-    MYSQL_ROW ROW; // Rows Handle
+    MYSQL_RES* result; // Resulting Table Handle
+    MYSQL_ROW row; // Rows Handle
     // Get a handle to the resulting table
-    RESULT = mysql_store_result(CONNECTION);
+    result = mysql_store_result(connection);
     if (RESULT == NULL)
         cout << "Error: can't get the result description\n";
     else
     {
-        if (mysql_num_rows(RESULT) > 0)
+        if (mysql_num_rows(result) > 0)
         {
             // Go through all the records of the resulting table
-            while ((ROW = mysql_fetch_row(RESULT)) != NULL)
-                cout << ROW[0] << " " << ROW[1] << endl;
+            while ((row = mysql_fetch_row(result)) != NULL)
+                cout << row[0] << " " << row[1] << endl;
         }
-        else if (mysql_num_rows(RESULT) == 0)
+        else if (mysql_num_rows(result) == 0)
             cout << "No user with these data.\n";
     }
     
     // Free up the memory used by the result table
-    mysql_free_result(RESULT);
+    mysql_free_result(result);
 }
 
 void Database::PutUserData(string sql_statement)
 {
     //Execute SQL-query
-    if (mysql_query(CONNECTION, sql_statement.c_str()) != 0)
+    if (mysql_query(connection, sql_statement.c_str()) != 0)
         cout << "Error: can't execute SQL-query\n";
     else
         cout << "The user is added.\n";
@@ -81,5 +81,5 @@ void Database::PutUserData(string sql_statement)
 Database::~Database()
 {
     // Closing the connection
-    mysql_close(CONNECTION);
+    mysql_close(connection);
 }
