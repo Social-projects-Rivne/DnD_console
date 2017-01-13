11b177f2cf663680265e8467e271daba635bff4e
Olha <olhalesk@gmail.com>
2017-01-13 16:21:52 +0200

    DataBase after the review.

---
+++ src/Server/Includes/DataBase.hpp
@@ -0,0 +1,30 @@
+//
+//  DataBase.hpp
+//  Olha Leskovs'ka
+//
+
+
+#ifndef DataBase_hpp
+#define DataBase_hpp
+
+using json = nlohmann::json;
+
+class DataBase
+{
+    MYSQL* connection; // Database Connection Handle/Decryptor
+    
+public:
+    DataBase();
+    
+    json fConnection(std::string host, std::string user_name, std::string password, std::string database_name);
+    
+    json fExecuteQuery(std::string sql_statement);
+    
+    json fGetData(std::string sql_statement);
+    
+    json fPutData(std::string sql_statement);
+    
+    ~DataBase();
+}; // class DataBase
+
+#endif /* DataBase_hpp */
