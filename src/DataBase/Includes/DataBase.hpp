38f90fb30d4109cca97e129afd9aeea1a412f9bc
Olha <olhalesk@gmail.com>
2017-01-16 21:18:23 +0200

    Final version of DataBase class

--- src/DataBase/Includes/DataBase.hpp
+++ src/DataBase/Includes/DataBase.hpp
@@ -17,13 +17,13 @@ private:
 public:
     DataBase();
     
-    json fConnection(std::string host, std::string user_name, std::string password, std::string database_name);
+    json fConnection(std::string host, std::string user_name, std::string password, std::string database_name); // for connection with database
     
-    json fExecuteQuery(std::string sql_statement);
+    json fExecuteQuery(std::string sql_statement); // for executing a quety
     
-    json fGetData(std::string sql_statement);
+    json fGetData(std::string sql_statement); // for getting Data from Database
     
-    json fPutData(std::string sql_statement);
+    json fPutData(std::string sql_statement); // for putting Data into Database
     
     ~DataBase();
 }; // class DataBase
